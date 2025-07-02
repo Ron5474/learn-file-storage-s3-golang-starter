e58c579eda7ad1f10f92d911102d815099d8c7ad
Ronak Patel <ronp2805@gmail.com>
2025-07-02 15:51:33 -0700

    Almost done

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -32,6 +39,70 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not upload image", err)
+		return
+	}
+
+	mediaType := header.Header.Get("Content-Type")
+
+	parsedType, _, err  := mime.ParseMediaType(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Internal Server Error", err)
+		return
+	}
+	if parsedType != "image/png" && parsedType != "image/jpg" {
+		respondWithError(w, http.StatusBadRequest, "Invalid Image Format", nil)
+		return
+	}
+	// imageData, err := io.ReadAll(file)
+
+	// encodedImage := base64.StdEncoding.EncodeToString(imageData)
+
+	// dataURL := fmt.Sprintf("data:%v;base64,%v", mediaType, encodedImage)
+
+	metadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Unable to locate Video", err)
+		return
+	}
+
+	if metadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
+		return
+	}
+
+	fileExtenstion := strings.Split(mediaType, "/")[1]
+	fileNameBytes := make([]byte, 32)
+	_, err = rand.Read(fileNameBytes)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Internal Server Error", err)
+		return
+	}
+
+	fileName := base64.RawURLEncoding.EncodeToString(fileNameBytes)
+
+	storagePath := filepath.Join(cfg.assetsRoot, fileName) + "." + fileExtenstion
+	imageFile, err := os.Create(storagePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Internal Server Error", err)
+		return
+	}
+	io.Copy(imageFile, file)
+
+	dataURL := fmt.Sprintf("http://localhost:%v/assets/%v.%v", cfg.port, fileName, fileExtenstion)
+	metadata.ThumbnailURL = &dataURL
+
+	err = cfg.db.UpdateVideo(metadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Internal Server Error", err)
+		return
+	}
+
+
+	respondWithJSON(w, http.StatusOK, metadata)
 }
